2c675a084f075cf69c4b9aa5e0c704cb04f17056
DmitryHit <8753026@gmail.com>
2022-06-29 11:01:00 +0300

    Lesson 1
    Submission of practical assignment number 1

---
+++ Lesson 1/1.1.py
@@ -0,0 +1,7 @@
+# 1)---------------------------------------------------------
+first = 0
+second_int1 = int(input("Введите число: "))
+second_int2 = int(input("Введите число: "))
+third = input("Введите какой то текст: ")
+print(first, second_int1, second_int2, third)
+
---
+++ Lesson 1/1.3.py
@@ -0,0 +1,8 @@
+# 3)---------------------------------------------------------
+n = int(input("Введите число: "))
+temp = str(n)
+t1 = temp + temp
+t2 = temp + temp + temp
+result = n + int(t1) + int(t2)
+print("Сумма чисел: ", result)
+
