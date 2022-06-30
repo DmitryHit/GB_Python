4ff255e8f0c2062a84272740d7e22c03ec710efc
DmitryHit <81321476+DmitryHit@users.noreply.github.com>
2022-06-30 17:15:01 +0300

    Update 1.3.py

--- Lesson 1/1.3.py
+++ Lesson 1/1.3.py
@@ -1,8 +1,8 @@
 # 3)---------------------------------------------------------
-n = int(input("Введите число: "))
-temp = str(n)
-t1 = temp + temp
-t2 = temp + temp + temp
-result = n + int(t1) + int(t2)
+num = int(input("Введите число: "))
+temp = str(num)
+temp1 = temp + temp
+temp2 = temp + temp + temp
+result = num + int(temp1) + int(temp2)
 print("Сумма чисел: ", result)
 
