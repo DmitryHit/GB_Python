919f3bb43221542713eb5300c0e22cc6c3cc4989
DmitryHit <81321476+DmitryHit@users.noreply.github.com>
2022-06-30 17:21:13 +0300

    Update 1.7.py

--- Lesson 1/1.7.py
+++ Lesson 1/1.7.py
@@ -1,11 +1,11 @@
 # 6)----------------------------------------------------------
-a = int(input("Введите результаты пробежки первого дня в км: "))
-b = int(input("Введите общий желаемый результат в км: "))
+result = int(input("Введите результаты пробежки первого дня в км: "))
+desired_result = int(input("Введите общий желаемый результат в км: "))
 day = 1
 print("Результат:")
-print(day, "-й день:", a)
-while b - a > 0:
-    a = a + (a * 0.1)
+print(day, "-й день:", result)
+while desired_result - result > 0:
+    result = result + (result * 0.1)
     day += 1
-    print(day, "-й день:", round(a, 2))
+    print(day, "-й день:", round(result, 2))
 print("Вы достигнете результата на", day, "-й день!")
