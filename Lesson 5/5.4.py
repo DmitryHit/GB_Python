57ff45c83caacbe092d0b9d96281723c25b04b60
DmitryHit <8753026@gmail.com>
2022-07-13 16:09:30 +0300

    Lesson 5
    литл правка

--- Lesson 5/5.4.py
+++ Lesson 5/5.4.py
@@ -6,5 +6,5 @@ with open('5.4.txt', 'r') as file:
         new_file.append(numerals[el[0]] + '  ' + el[1])
     print(new_file)
 
-with open('5.4_new.txt', 'w') as file_2:
+with open('5.4_new.txt', 'w', encoding='utf-8') as file_2:
     file_2.writelines(new_file)
